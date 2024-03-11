72a33589bc1af7a071b0a9b49632c01c26a409ca
sukmaaji11 <sukmaaji11@gmail.com>
2024-03-11 14:45:41 +0700

    Connect to Database

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark: text-white"
         to="/"
       >
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+        <span className="px-2 py-1 bg-gradient-to-r from-green-700 to-blue-500 rounded-lg text-white">
           Jadi Cuan
         </span>
       </Link>
@@ -32,7 +32,7 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to="signin">
-          <Button gradientDuoTone="purpleToBlue">Masuk </Button>
+          <Button gradientDuoTone="greenToBlue">Masuk </Button>
         </Link>
         <Navbar.Toggle />
       </div>
