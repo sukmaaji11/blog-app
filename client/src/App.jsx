3f4d9af1632b6b39a24d7597e3fb4a0f2d6b35e4
Tech Arkademi <arkad@tech-mediaa.local.arkd>
2024-03-11 11:10:16 +0700

    Create Pages and Routes for Blog-App

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Singup from "./pages/Singup";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Singup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
