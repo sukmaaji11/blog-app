8667689e19cd23c16109c837cc7706fd9897e32f
sukmaaji11 <sukmaaji11@gmail.com>
2024-04-02 23:46:25 +0700

    Complete Sidebar

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DSidebar from "../components/DSidebar";
+import DProfile from "../components/DProfile";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromURL = urlParams.get("tab");
+    if (tabFromURL) {
+      setTab(tabFromURL);
+    }
+  }, [location.search]);
+  return (
+    <div>
+      <div className="">
+        {/*Sidebar*/}
+        <DSidebar />
+      </div>
+      {/*Profile */}
+      {tab === "profile" && <DProfile />}
+    </div>
+  );
 }
