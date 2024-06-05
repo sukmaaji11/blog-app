0d787f8146ac41982d0b2954ba882355fcb663b4
sukmaaji11 <sukmaaji11@gmail.com>
2024-06-06 00:13:11 +0700

    Post API Create

---
+++ api/models/post.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        "https://i.pinimg.com/564x/93/f7/53/93f753264d380b7648ad54d8cd492ed2.jpg",
+    },
+    category: {
+      type: String,
+      default: "uncategorized",
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+export default Post;
